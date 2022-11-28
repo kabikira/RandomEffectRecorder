5741ea35ed7a8f3ae22c7bca53a64f310162686a
kabikira <kabikirar2@protonmail.com>
2022-11-28 23:07:11 +0900

    Initial Commit

---
+++ R.E.R/R_E_RApp.swift
@@ -0,0 +1,17 @@
+//
+//  R_E_RApp.swift
+//  R.E.R
+//
+//  Created by koala panda on 2022/11/28.
+//
+
+import SwiftUI
+
+@main
+struct R_E_RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
